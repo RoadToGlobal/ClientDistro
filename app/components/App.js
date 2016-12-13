2b801c75e8c2c51859556f1d3072419a0eed32e9
goodoltoby <tob.jan87@gmail.com>
2016-12-13 18:22:02 +0100

    rebased from master

--- app/components/App.js
+++ app/components/App.js
@@ -2,14 +2,16 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 import SideBar from '../containers/SideBar';
+import Login from '../containers/Login';
 import { verticalContainer, horizontalContainer } from '../DynamicStyles/common';
 
 const App = ({ children }) =>
   <div style={globalContainer}>
     <SideBar />
     <div style={mainContainer}>
-      <header style={{paddingLeft: '1em'}}>
+      <header style={headerContainer}>
         <Link to="/" style={brandName}><h1>GroupApp!</h1></Link>
+        <Login />
       </header>
       { children }
     </div>
@@ -17,6 +19,10 @@ const App = ({ children }) =>
 
 App.propTypes = {
   children: PropTypes.object,
+};
+
+const headerContainer = {
+  ...verticalContainer,
 };
 
 const globalContainer = {
