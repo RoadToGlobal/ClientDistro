e797cfe02c431b304bd33505e71107ee46b373aa
goodoltoby <tob.jan87@gmail.com>
2016-12-13 21:29:23 +0100

    Quickfix structure

--- app/components/SideBarIcon.js
+++ app/components/SideBarIcon.js
@@ -1,5 +1,42 @@
 import React, { PropTypes } from 'react';
 
+const icon = {
+  marginBottom: 10.5,
+  cursor: 'pointer',
+  borderLeft: '5px solid ' + this.props.activity.customization.backgroundColor,
+};
+const iconItem = {
+  ...icon,
+  opacity: 0.5,
+  transition: 'all 0.5s',
+};
+const iconItemHover = {
+  ...icon,
+  opacity: 1,
+  transition: 'all 0.5s',
+};
+const iconImg = {
+  display: 'block',
+  margin: '0 auto',
+  marginBottom: 5,
+  width: 50,
+  transition: 'all 0.2s',
+};
+const iconImgHover = {
+  ...iconImg,
+  width: 75,
+};
+const iconTxt = {
+  display: 'block',
+  fontSize: 12,
+  textAlign: 'center',
+  transition: 'all 0.2s',
+};
+const iconTxtHover = {
+  ...iconTxt,
+  fontSize: 14,
+};
+
 class SideBarIcon extends React.Component {
   static propTypes = {
     activity: PropTypes.object,
@@ -13,48 +50,6 @@ class SideBarIcon extends React.Component {
   }
 
   render() {
-    const icon = {
-      marginBottom: 10.5,
-      cursor: 'pointer',
-      borderLeft: '5px solid ' + this.props.activity.customization.backgroundColor,
-    };
-    const iconItem = {
-      ...icon,
-      opacity: 0.5,
-      transition: 'all 0.5s',
-    };
-
-    const iconItemHover = {
-      ...icon,
-      opacity: 1,
-      transition: 'all 0.5s',
-    };
-
-    const iconImg = {
-      display: 'block',
-      margin: '0 auto',
-      marginBottom: 5,
-      width: 50,
-      transition: 'all 0.2s',
-    };
-
-    const iconImgHover = {
-      ...iconImg,
-      width: 75,
-    };
-
-    const iconTxt = {
-      display: 'block',
-      fontSize: 12,
-      textAlign: 'center',
-      transition: 'all 0.2s',
-    };
-
-    const iconTxtHover = {
-      ...iconTxt,
-      fontSize: 14,
-    };
-
     return (
       <div
         style={this.props.hoverStatus ? iconItemHover : iconItem}
