1145da4d020ad09379a4dd1b79ad65b3e17e18f7
storkfan <reinholdsson.andreas@gmail.com>
2016-12-14 14:55:34 +0100

    implemented FFFWindow on all views

--- app/views/ActivityStatus.js
+++ app/views/ActivityStatus.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { FFFWindowStyle } from '../DynamicStyles/common';
+import { FFFContainer} from '../DynamicStyles/common';
+import FFFWindow from '../containers/FFFWindow';
 
 export default class Group extends React.Component {
   render() {
     return (
-      <div style={FFFWindowStyle}>
-        <p>
-          placeholder
-        </p>
+      <div style={FFFContainer}>
+        <FFFWindow title={'Activity Status'} header={Boolean(true)}>
+          <p>
+            pLaCeHoLdEr
+          </p>
+        </FFFWindow>
       </div>
    	);
   }
