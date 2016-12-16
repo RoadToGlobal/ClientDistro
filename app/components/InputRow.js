f542a19a1153c53afbfec1878119c85ae24b4c51
storkfan <reinholdsson.andreas@gmail.com>
2016-12-16 16:40:47 +0100

    added components for future implementation

---
+++ app/components/InputRow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MagicInput from './MagicInput';
+
+
+class InputRow extends React.Component {
+  render() {
+    return (
+      <div>
+        <MagicInput />
+      </div>
+    );
+  }
+}
+
+export default InputRow;
