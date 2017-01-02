d4f83ea0654f5f2b1fe4c7cf076693ead96bb306
storkfan <reinholdsson.andreas@gmail.com>
2017-01-02 18:55:16 +0100

    Fixed window size

--- app/views/About.js
+++ app/views/About.js
@@ -12,7 +12,7 @@ export default class FAQ extends React.Component {
     return (
       <div style={FFFContainer}>
         <FFFWindow title={'About'} header={Boolean(true)} specStyle={{height: '18em'}}>
-          <p>Our vision started as a school project and is now on it's way to be something alot bigger. The idea comes from the need of grouping people up with each other even though they don't know each other, all this based on their biggest interests. In this way you will always find someone to share your interests with. We will say this again and again...</p>
+          <p>Our vision started as a school project and is now on it's way to be something a lot bigger. The idea comes from the need of grouping people up with each other even though they don't know each other, all this based on their biggest interests. In this way you will always find someone to share your interests with. We will say this again and again...</p>
           <p>Do meet strangers!</p>
           <h3 style={aboutBigText}>If you encounter any bugs you may feel free to report them to us at <a href="#">groupmakerapp@gmail.com</a>.</h3>
 
